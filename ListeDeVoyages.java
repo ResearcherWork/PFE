4b1329b1a363c7342e631f8b485a8d2985831308
Tesnime <tesnimetouil8@gmail.com>
2017-02-06 20:05:03 +0100

    Update ListeDeVoyages.java

--- ListeDeVoyages.java
+++ ListeDeVoyages.java
@@ -12,6 +12,44 @@ package AlgorithmeEvaluation;
 public class ListeDeVoyages {
     
     
-    Voyage v = new Voyage();
+    
+    private ArrayList<Voyage> AV = new ArrayList <Voyage> ();
+    
+     public ListeDeVoyages() {
+    }
+
+    public ListeDeVoyages(ArrayList AV) {
+        this.AV = AV;
+    }
+    
+   public ArrayList getAV() {
+        return AV;
+    }
+
+    public void setAV(ArrayList AV) {
+        this.AV = AV;
+    }
+    public ArrayList<Voyage> ListV()
+    {
+     System.out.println("Donner le nombre de voyages");
+     Scanner sc = new Scanner(System.in);
+     int nb = sc.nextInt();
+     System.out.println("Donner la station de depart");
+     Scanner scc = new Scanner(System.in);
+     String sd = scc.nextLine();
+     System.out.println("Donner la station d'arrivée");
+     Scanner sccc = new Scanner(System.in);
+     String sa = sccc.nextLine();
+     System.out.println("Donner le temps de depart");
+     Scanner scccc = new Scanner(System.in);
+     int td = scccc.nextInt();
+     for(int i=0;i<nb;i++)
+    {
+      AV.add(new Voyage(sd,sa,td));
+      
+    }
+     return AV;
+     
+    }
     
 }
